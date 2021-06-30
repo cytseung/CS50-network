aa2f781f8b2fd10e07ffb56a01d75988962cc1b6
cyt <NA>
2021-06-30 16:51:23 +0800

    Comments component

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Comments = ({ comments }) => {
     return (
-        comments.map(comment=>{
-            return (<p>{comment}</p>);
+        comments.map(comment => {
+            const d = new Date(comment.createdOn);
+            return (
+                <>
+                    <p>{comment.text}</p>
+                    <p>{comment.username}&nbsp;{d.toLocaleDateString()}&nbsp;{d.toLocaleTimeString()}</p>
+                </>
+            );
         })
-
-        
     )
 }
 
